8b18451618b97d3f2283fe5b225b7c52ba6c765c
Marcos Vinícius Moraes Costa <81113421+MarcosVini753@users.noreply.github.com>
2022-07-28 14:34:35 -0500

    Update Envelopes1.java

--- java/pj-2009-f1/Envelopes1.java
+++ java/pj-2009-f1/Envelopes1.java
@@ -2,18 +2,26 @@ import java.util.Scanner;
 
 public class Envelopes1 {
    public static void main(String[] args) {
+      // https://olimpiada.ic.unicamp.br/pratique/pj/2009/f1/envelopes1/
       Scanner in = new Scanner(System.in);
 
-      int n = in.nextInt(), r = in.nextInt();
+      int tiposEnvelope = in.nextInt();
+      // devemos imprimir o menor número dentre os digitados
+      // pois este representa a quantidade de rotulos que podemos trocar
+      int menor = in.nextInt();
+      // consideramos a primeira quantidade de rotulos digitada como a menor
 
-      for (int i = 1; i < n; i++) {
-         int aux = in.nextInt();
-         if (aux < r) {
-            r = aux;
+      for (int i = 1; i < tiposEnvelope; i++) {
+
+         int qntRotulo = in.nextInt();
+         // procuramos um rotulo que esteja em uma quantidade menor
+         // do que a anterior e guardamos essa quantidade
+         if (qntRotulo < menor) {
+            menor = qntRotulo;
          }
       }
 
-      System.out.println(r);
+      System.out.println(menor);
       in.close();
    }
 }
