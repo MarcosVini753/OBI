9eb20808d3dcb7f69f5e5e96e4186166174d502e
Marcos Vinícius Moraes Costa <81113421+MarcosVini753@users.noreply.github.com>
2022-07-28 14:17:43 -0500

    Update Vestibular.java

--- java/pj-2008-f1/Vestibular.java
+++ java/pj-2008-f1/Vestibular.java
@@ -2,17 +2,20 @@ import java.util.Scanner;
 
 public class Vestibular {
    public static void main(String[] args) {
+      // https://olimpiada.ic.unicamp.br/pratique/pj/2008/f1/vestib/
       Scanner in = new Scanner(System.in);
-      int pts = 0, n = Integer.parseInt(in.nextLine());
-      String g = in.nextLine();
-      String c = in.nextLine();
 
-      for (int i = 0; i < n; i++) {
-         if (g.charAt(i) == c.charAt(i)) {
-            pts++;
+      int numeroQuestoes = Integer.parseInt(in.nextLine()), acertos = 0;
+      String gabarito = in.nextLine();
+      String questoesCandidato = in.nextLine();
+
+      for (int i = 0; i < numeroQuestoes; i++) {
+
+         if (gabarito.charAt(i) == questoesCandidato.charAt(i)) {
+            acertos++;
          }
       }
-      System.out.println(pts);
+      System.out.println(acertos);
       in.close();
    }
 }
