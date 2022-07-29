d4288879c8a78bfdf1d25c1248ea9d08e5176d87
Marcos Vinícius Moraes Costa <81113421+MarcosVini753@users.noreply.github.com>
2022-07-29 15:37:04 -0500

    Update Vestibular.c

--- junior/2008/fase1/Vestibular.c
+++ junior/2008/fase1/Vestibular.c
@@ -1,18 +1,15 @@
 #include <stdio.h>
-#include <string.h>
-
-int main(void)
-{
+// https://olimpiada.ic.unicamp.br/pratique/pj/2008/f1/vestib/
+int main(void){
    int numeroQuestoes, pontos = 0;
    scanf("%d", &numeroQuestoes);
 
    char gabarito[82], candidato[82];
    scanf(" %s %s", gabarito, candidato);
 
-   for (int i = 0; i < numeroQuestoes; i++)
-   {
-      if (gabarito[i] == candidato[i])
-      {
+   for (int i = 0; i < numeroQuestoes; i++){
+      
+      if (gabarito[i] == candidato[i]){
          pontos++;
       }
    }
