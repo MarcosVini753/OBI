6f05b976772fd145c631bc78cc38282a02d1f1a4
Marcos Vinícius Moraes Costa <81113421+MarcosVini753@users.noreply.github.com>
2022-07-29 15:59:00 -0500

    Create telefone.c

---
+++ junior/2008/fase1/telefone.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <string.h>
+// https://olimpiada.ic.unicamp.br/pratique/pj/2008/f1/telefone2/
+int main(void){
+   char telefone[20], telefoneTraduzido[20];
+   scanf(" %s", telefone);
+  
+   for (int i = 0; i < strlen(telefone); i++){
+     
+      if (telefone[i] > 'V'){
+         telefoneTraduzido[i] = '9';
+      }
+      else if (telefone[i] > 'S'){
+         telefoneTraduzido[i] = '8';
+      }
+      else if (telefone[i] > 'O'){
+         telefoneTraduzido[i] = '7';
+      }
+      else if (telefone[i] > 'L'){
+         telefoneTraduzido[i] = '6';
+      }
+      else if (telefone[i] > 'I'){
+         telefoneTraduzido[i] = '5';
+      }
+      else if (telefone[i] > 'F'){
+         telefoneTraduzido[i] = '4';
+      }
+      else if (telefone[i] > 'C'){
+         telefoneTraduzido[i] = '3';
+      }
+      else if (telefone[i] >= 'A'){
+         telefoneTraduzido[i] = '2';
+      }
+      else{
+         telefoneTraduzido[i] = telefone[i];
+      }
+     
+      telefoneTraduzido[i + 1] = '\0';
+   }
+
+   printf("%s", telefoneTraduzido);
+   return 0;
+}
