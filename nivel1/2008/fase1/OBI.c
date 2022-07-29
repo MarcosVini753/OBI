b37f3989d8a1a3ab379c7508ce62c37ee6b22513
Marcos Vinícius Moraes Costa <81113421+MarcosVini753@users.noreply.github.com>
2022-07-29 16:33:24 -0500

    Update OBI.c

--- nivel1/2008/fase1/OBI.c
+++ nivel1/2008/fase1/OBI.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
 // https://olimpiada.ic.unicamp.br/pratique/p1/2008/f1/obi/
-int main(void)
-{
+int main(void){
    int numeroCompetidores, pontosMinimos, competidoresConvidados = 0;
    scanf("%d %d", &numeroCompetidores, &pontosMinimos);
-   for (int i = 0; i < numeroCompetidores; i++)
-   {
+   for (int i = 0; i < numeroCompetidores; i++){
+      
       int ponto1, ponto2;
       scanf("%d %d", &ponto1, &ponto2);
-      if (ponto1 + ponto2 >= pontosMinimos)
-      {
+      
+      if (ponto1 + ponto2 >= pontosMinimos){
          competidoresConvidados++;
       }
    }
